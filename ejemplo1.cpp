f69fd2c4aecba07e75e2e05b75b8dc2b21a68fa6
aquispeg <aquispeg@unjbg.edu.pe>
2025-06-18 22:06:19 -0500

    actualizar ejemplo1:hallar el empleado con mayores ventas

--- ejemplo1.cpp
+++ ejemplo1.cpp
@@ -24,17 +24,22 @@ int main (){
         cout<<" el salario del empleado: ";
         cin>>EMPLEADOS[i].sal;    
     }
-
-    cout<<"DATOS INGRESADOS"<<endl;
-    for (int i=0;i<n;i++){
-        cout<<"numero: "<<EMPLEADOS[i].num<<endl;
-        cout<<"nombre: "<<EMPLEADOS[i].nom<<endl;
-        cout<<"salario: "<<EMPLEADOS[i].sal<<endl;
-        cout<<"ventas por mes: ";
-        for (int j=0;j<12;j++){
-            cout<<EMPLEADOS[i].ven[j]<< " ";
+    int posmax=0;
+    float maxventas=0;
+    for(int i=0;i<n;i++){
+        float sumaven=0;
+        for(int j=0;j<12;j++){
+            sumaven=sumaven+EMPLEADOS[i].ven[j];
         }
-        cout<<endl;
+        if(sumaven > maxventas){
+            maxventas=sumaven;
+            posmax=i;
+        }
     }
+    cout<<"EMPLEADO CON MAYORES VENTAS"<<endl;
+    cout<<"numero: "<<EMPLEADOS[posmax].num<<endl;
+    cout<<"nombre: "<<EMPLEADOS[posmax].nom<<endl;
+    cout<<"ventas totales: "<<maxventas<<endl;
+    
     return 0;
 }
